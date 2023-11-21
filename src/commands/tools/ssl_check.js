9ac819953a69552fe0a70b88989a7edeb2e2aabe
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2023-11-20 23:53:54 -0500

    v1.4.0
    Corrected the sslcheck command code to handle empty spaces in the date fields as that causes the command to error out when checking sites that have formatting issues.
    
    I also updated the dependencies.

--- src/commands/tools/ssl_check.js
+++ src/commands/tools/ssl_check.js
@@ -1,80 +1,74 @@
-const { PermissionFlagsBits, PermissionsBitField, SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const https = require('https');
-const { parse, format, differenceInDays } = require('date-fns');
+const { parse, differenceInDays } = require('date-fns');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('sslcheck')
-    .setDescription('Returns the expiry date of the SSL certificate for a website.')
+    .setDescription('Returns the SSL certificate status for a website.')
     .addStringOption(option =>
       option.setName('website')
         .setDescription('The URL of the website to check')
         .setRequired(true)),
 
   async execute(interaction, client) {
-
-    //fetches the ninja role ID to be used later in the command.
-
     const { roles } = interaction.member;
-    const role = await interaction.guild.roles
-      .fetch("939318588605603850")
-      .catch(console.error);
 
-    //cross-references if the user running the command has the ninja role. Otherwise, state they lack the ninja role.
+    // Fetch the guild to access roles and client
+    const guild = await client.guilds.fetch(interaction.guildId).catch(console.error);
 
+    // Cross-references if the user running the command has the ninja role. Otherwise, state they lack the ninja role.
     if (roles.cache.has("939318588605603850")) {
-    try {
-      const websiteURL = interaction.options.getString('website');
-      const req = https.request(websiteURL, (res) => {
-        const certificate = res.socket.getPeerCertificate();
-        const expirationDateStr = certificate.valid_to; // Get the date string
-        console.log('Certificate Data:', certificate);
+      try {
+        await interaction.deferReply({ ephemeral: true });
 
-        // Convert the date string to a JavaScript Date object
-        const expirationDate = parse(expirationDateStr, 'MMM dd HH:mm:ss yyyy \'GMT\'', new Date());
+        const websiteURL = interaction.options.getString('website');
 
-        if (!isNaN(expirationDate)) {
-          const daysRemaining = differenceInDays(expirationDate, new Date());
-          if (daysRemaining <= 7) {
-            interaction.reply({
-              content: `:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days.`,
-              ephemeral: true, // Only visible to the user who triggered the command
+        const checkCertificate = () => {
+          return new Promise((resolve, reject) => {
+            const req = https.request(websiteURL, (res) => {
+              const certificate = res.socket.getPeerCertificate();
+              const expirationDateStr = certificate.valid_to; // Get the date string
+
+              // Convert the date string to a JavaScript Date object
+              const formattedDateStr = expirationDateStr.replace(/\s+/g, ' '); // Remove extra spaces
+              const expirationDate = parse(formattedDateStr, 'MMM d HH:mm:ss yyyy \'GMT\'', new Date());
+
+              if (!isNaN(expirationDate)) {
+                const daysRemaining = differenceInDays(expirationDate, new Date());
+
+                if (daysRemaining > 8 && daysRemaining < 14) {
+                  resolve(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days.`);
+                } else if (daysRemaining < 7) {
+                  resolve(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days. Please renew the SSL Certificate to prevent users from being antsy!`);
+                } else {
+                  resolve(`:white_check_mark: SSL certificate for ${websiteURL} is valid and not expiring soon.`);
+                }
+              } else {
+                reject(':x: Unable to retrieve SSL certificate information or parse the date.');
+              }
             });
-          } else {
-            interaction.reply({
-              content: `:white_check_mark: SSL certificate for ${websiteURL} is valid and not expiring soon.`,
-              ephemeral: true, // Only visible to the user who triggered the command
+
+            req.on('error', (error) => {
+              console.error('An error occurred while checking the SSL certificate:', error);
+              reject(':x: An error occurred while checking the SSL certificate.');
             });
-          }
-        } else {
-          interaction.reply({
-            content: ':x: Unable to retrieve SSL certificate information.',
-            ephemeral: true, // Only visible to the user who triggered the command
+
+            req.end();
           });
-        }
-      });
+        };
 
-      req.on('error', (error) => {
+        const result = await checkCertificate();
+        await interaction.editReply({ content: result, ephemeral: true });
+      } catch (error) {
         console.error('An error occurred while checking the SSL certificate:', error);
-        interaction.reply({
-          content: ':x: The SSL certificate for the website has already expired.',
-          ephemeral: true, // Only visible to the user who triggered the command
-        });
-      });
-
-      req.end();
-    } catch (error) {
-      console.error('An error occurred while checking the SSL certificate:', error);
-      interaction.reply({
-        content: ':x: An error occurred while checking the SSL certificate.',
-        ephemeral: true, // Only visible to the user who triggered the command
+        await interaction.editReply({ content: ':x: An error occurred while checking the SSL certificate.', ephemeral: true });
+      }
+    } else {
+      await interaction.reply({
+        content: `You do not have the Ninja role.`,
+        ephemeral: true,
       });
     }
-  } else {
-    await interaction.reply({
-      content: `You do not have the Ninja role.`,
-      ephemeral: true,
-    });
-  }
   },
 };
