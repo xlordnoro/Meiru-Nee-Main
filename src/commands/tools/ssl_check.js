2316ec7d1d7013a5bdcf104e8e0ae35bc400ce55
xlordnoro <69708902+xlordnoro@users.noreply.github.com>
2024-03-05 02:48:51 -0500

    v1.7
    - I switched from https to the tls library to handle SSL certificate expiry checks as https was causing errors to occur.
    
    - Resulting in crashes and no warnings being pushed to the required channels.
    
    - Lastly, I updated all the dependencies to their latest versions.

--- src/commands/tools/ssl_check.js
+++ src/commands/tools/ssl_check.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const https = require('https');
+const tls = require('tls');
 const { parse, differenceInDays } = require('date-fns');
 
 module.exports = {
@@ -17,44 +17,40 @@ module.exports = {
     // Fetch the guild to access roles and client
     const guild = await client.guilds.fetch(interaction.guildId).catch(console.error);
 
-    // Cross-references if the user running the command has the ninja role. Otherwise, state they lack the ninja role.
+    // Cross-references if the user running the command has the developer role. Otherwise, state they lack the developer role.
     if (roles.cache.has("939318588605603850")) {
       try {
         await interaction.deferReply({ ephemeral: true });
 
         const websiteURL = interaction.options.getString('website');
 
-        const checkCertificate = () => {
-          return new Promise((resolve, reject) => {
-            const req = https.request(websiteURL, (res) => {
-              const certificate = res.socket.getPeerCertificate();
-              const expirationDateStr = certificate.valid_to; // Get the date string
+        const checkCertificate = async () => {
+          return new Promise(async (resolve, reject) => {
+            const socket = tls.connect(443, websiteURL, { rejectUnauthorized: false }, () => {
+              const certificate = socket.getPeerCertificate(true);
+              socket.end();
 
-              // Convert the date string to a JavaScript Date object
-              const formattedDateStr = expirationDateStr.replace(/\s+/g, ' '); // Remove extra spaces
-              const expirationDate = parse(formattedDateStr, 'MMM d HH:mm:ss yyyy \'GMT\'', new Date());
+              if (certificate && Object.keys(certificate).length > 0) {
+                const expirationDateStr = certificate.valid_to; // Get the date string
+                const expirationDate = parse(expirationDateStr, 'MMM d HH:mm:ss yyyy \'GMT\'', new Date());
 
-              if (!isNaN(expirationDate)) {
-                const daysRemaining = differenceInDays(expirationDate, new Date());
+                if (!isNaN(expirationDate)) {
+                  const daysRemaining = differenceInDays(expirationDate, new Date());
 
-                if (daysRemaining > 8 && daysRemaining < 14) {
-                  resolve(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days.`);
-                } else if (daysRemaining < 7) {
-                  resolve(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days. Please renew the SSL Certificate to prevent users from being antsy!`);
+                  if (daysRemaining > 8 && daysRemaining < 14) {
+                    resolve(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days.`);
+                  } else if (daysRemaining < 7) {
+                    resolve(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days. Please renew the SSL Certificate to prevent users from being antsy!`);
+                  } else {
+                    resolve(`:white_check_mark: SSL certificate for ${websiteURL} is valid and not expiring soon.`);
+                  }
                 } else {
-                  resolve(`:white_check_mark: SSL certificate for ${websiteURL} is valid and not expiring soon.`);
+                  reject(':x: Unable to parse SSL certificate expiration date.');
                 }
               } else {
-                reject(':x: Unable to retrieve SSL certificate information or parse the date.');
+                reject(':x: SSL certificate information is empty or incomplete.');
               }
             });
-
-            req.on('error', (error) => {
-              console.error('An error occurred while checking the SSL certificate:', error);
-              reject(':x: An error occurred while checking the SSL certificate.');
-            });
-
-            req.end();
           });
         };
 
@@ -66,7 +62,7 @@ module.exports = {
       }
     } else {
       await interaction.reply({
-        content: `You do not have the Ninja role.`,
+        content: `You do not have the Developer role.`,
         ephemeral: true,
       });
     }
--- src/functions/tools/sslCheck2.js
+++ src/functions/tools/sslCheck2.js
@@ -1,81 +1,79 @@
 const https = require('https');
-const { parse, format, differenceInDays } = require('date-fns');
+const { parse, differenceInDays } = require('date-fns');
 const cron = require('node-cron');
 const { EmbedBuilder } = require('discord.js');
+const tls = require('tls');
 
 // Replace with your website URL
-const websiteURL = 'https://hi10anime.com';
+const websiteURL = 'hi10anime.com';
 
 // Function to execute the SSL check command
 module.exports = async (client) => {
   const executeSslCheckCommand = async () => {
     try {
-      const req = https.request(websiteURL, async (res) => {
-        const certificate = res.socket.getPeerCertificate();
-        const expirationDateStr = certificate.valid_to; // Get the date string
-        console.log('Certificate Data:', certificate);
+      await (async () => {
+        const socket = tls.connect(443, websiteURL, { rejectUnauthorized: false }, async () => {
+          const certificate = socket.getPeerCertificate(true);
+          socket.end();
 
-        // Convert the date string to a JavaScript Date object
-        const formattedDateStr = expirationDateStr.replace(/\s+/g, ' '); // Remove extra spaces
-        const expirationDate = parse(formattedDateStr, 'MMM d HH:mm:ss yyyy \'GMT\'', new Date());
+          if (certificate && Object.keys(certificate).length > 0) {
+            const expirationDateStr = certificate.valid_to; // Get the date string
+            const expirationDate = parse(expirationDateStr, 'MMM d HH:mm:ss yyyy \'GMT\'', new Date());
 
-        if (!isNaN(expirationDate)) {
-          const daysRemaining = differenceInDays(expirationDate, new Date());
+            if (!isNaN(expirationDate)) {
+              const daysRemaining = differenceInDays(expirationDate, new Date());
 
-          const guild = await client.guilds.fetch('155549815466491904').catch(console.error);
-          if (!guild) {
-            console.error(`:x: The specified guild (${guild}) is not found.`);
-            return;
-          }
+              const guild = await client.guilds.fetch('155549815466491904').catch(console.error);
+              if (!guild) {
+                console.error(`:x: The specified guild (${guild}) is not found.`);
+                return;
+              }
 
-          const channel = await guild.channels.fetch('1158250113941114901').catch(console.error);
-          if (!channel) {
-            console.error(`:x: The specified channel (${channel}) is not found.`);
-            return;
-          }
+              const channel = await guild.channels.fetch('1158250113941114901').catch(console.error);
+              if (!channel) {
+                console.error(`:x: The specified channel (${channel}) is not found.`);
+                return;
+              }
 
-          if (daysRemaining > 8 && daysRemaining < 14) {
-            // Prepare the embed for the "8 to 14 days" condition
-            const embed = new EmbedBuilder()
-              .setTitle('SSL Certificate Status')
-              .setDescription(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days.`)
-              .setColor('#3498db');
+              if (daysRemaining > 8 && daysRemaining < 14) {
+                // Prepare the embed for the "8 to 14 days" condition
+                const embed = new EmbedBuilder()
+                  .setTitle('SSL Certificate Status')
+                  .setDescription(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days.`)
+                  .setColor('#3498db');
 
-            // Send the embed with mentions to multiple roles as a single string with spaces
-            await channel.send({
-              embeds: [embed],
-              content: '<@&320836865047527426> <@&1053914574631477268>',
-              allowedMentions: { roles: ['320836865047527426', '1053914574631477268'] },
-            });
-            console.log('Embed sent successfully for 8 to 14 days condition!');
-          } else if (daysRemaining < 7) {
-            // Prepare the embed for the "less than 7 days" condition
-            const embed2 = new EmbedBuilder()
-              .setTitle('SSL Certificate Status')
-              .setDescription(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days. Please renew the SSL Certificate to prevent users from being antsy!`)
-              .setColor('#3498db');
+                // Send the embed with mentions to multiple roles as a single string with spaces
+                await channel.send({
+                  embeds: [embed],
+                  content: '<@&320836865047527426> <@&1053914574631477268>',
+                  allowedMentions: { roles: ['320836865047527426', '1053914574631477268'] },
+                });
+                console.log('Embed sent successfully for 8 to 14 days condition!');
+              } else if (daysRemaining < 7) {
+                // Prepare the embed for the "less than 7 days" condition
+                const embed2 = new EmbedBuilder()
+                  .setTitle('SSL Certificate Status')
+                  .setDescription(`:warning: SSL certificate for ${websiteURL} will expire in ${daysRemaining} days. Please renew the SSL Certificate to prevent users from being antsy!`)
+                  .setColor('#3498db');
 
-            // Send the embed with mentions to multiple roles as a single string with spaces
-            await channel.send({
-              embeds: [embed2],
-              content: '<@&320836865047527426> <@&1053914574631477268>',
-              allowedMentions: { roles: ['320836865047527426', '1053914574631477268'] },
-            });
-            console.log('Embed sent successfully for less than 7 days condition!');
+                // Send the embed with mentions to multiple roles as a single string with spaces
+                await channel.send({
+                  embeds: [embed2],
+                  content: '<@&320836865047527426> <@&1053914574631477268>',
+                  allowedMentions: { roles: ['320836865047527426', '1053914574631477268'] },
+                });
+                console.log('Embed sent successfully for less than 7 days condition!');
+              } else {
+                console.log(`:white_check_mark: SSL certificate for ${websiteURL} is valid and not expiring soon.`);
+              }
+            } else {
+              console.error(':x: Unable to parse SSL certificate expiration date.');
+            }
           } else {
-            console.log(`:white_check_mark: SSL certificate for ${websiteURL} is valid and not expiring soon.`);
+            console.error(':x: SSL certificate information is empty or incomplete.');
           }
-        } else {
-          console.error(':x: Unable to retrieve SSL certificate information or parse the date.');
-        }
-      });
-
-      req.on('error', (error) => {
-        console.error('An error occurred while checking the SSL certificate:', error);
-        console.log(':x: The SSL certificate for the website has already expired.');
-      });
-
-      req.end();
+        });
+      })();
     } catch (error) {
       console.error('An error occurred while checking the SSL certificate:', error);
       console.log(':x: An error occurred while checking the SSL certificate.');
